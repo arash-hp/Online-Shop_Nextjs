fa55bafdd905d7289276a0a9b170e7308061333b
Arash HP <arash.hpb@gmail.com>
2022-10-23 16:31:02 +0330

    Create sign in with formik and create config redux&tailwind

---
+++ app/components/form/Input.tsx
@@ -0,0 +1,25 @@
+import { Field, ErrorMessage } from "formik"
+import { FC } from "react"
+interface InputProps {
+    name: string,
+    type?: string,
+    label: string,
+    labelClassName?: string,
+    inputClassName?: string,
+    errorClassName?: string
+}
+export const Input: FC<InputProps> = ({
+    name,
+    label,
+    type = 'text',
+    labelClassName,
+    inputClassName,
+    errorClassName,
+}) => {
+
+    return <>
+        <label htmlFor={name} className={` ${labelClassName ?? ''}`}>{label}</label>
+        <Field id={name} name={name} type={type} className={`relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm ${inputClassName ?? ''}`} placeholder={`${name}`} />
+        <ErrorMessage name={name} className={`text-res-500 ${errorClassName ?? ''}`} />
+    </>
+}

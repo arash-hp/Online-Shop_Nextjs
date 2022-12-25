f022963b21ae25a4812a69cd2ea1b10aa483cb6c
Arash Hp <arash.hpb@gamil.com>
2022-12-25 13:07:12 +0330

    Create table for Admin_product

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -1,14 +1,108 @@
 import AdminPanelLayout from "../../../app/components/adminPanelLayout";
 import { NextPageWithLayout } from "../../_app";
 
-const ProductList : NextPageWithLayout = () => {
-    return (
-        <div>
-            <h1>Product`s Page</h1>
-        </div>
-    )
-}
+const ProductList: NextPageWithLayout = () => {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h1>Product`s Page</h1>
+        <button
+          type="button"
+          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          Add
+        </button>
+      </div>
 
-ProductList.getLayout = (page) => <AdminPanelLayout>{page}</AdminPanelLayout>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full">
+                <thead className="bg-white border-b">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-right"
+                    >
+                      #
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-right"
+                    >
+                      First
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-right"
+                    >
+                      Last
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-right"
+                    >
+                      Handle
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="bg-gray-100 border-b">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      1
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      Mark
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      Otto
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <a href="">Delete </a>
+                      <a href="">Edit</a>
+                    </td>
+                  </tr>
+                  <tr className="bg-white border-b">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      2
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      Jacob
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      Thornton
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <a href="">Delete </a>
+                      <a href="">Edit</a>
+                    </td>
+                  </tr>
+                  <tr className="bg-gray-100 border-b">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      3
+                    </td>
+                    <td
+                      colSpan={2}
+                      className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center"
+                    >
+                      Larry the Bird
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <a href="">Delete </a>
+                      <a href="">Edit</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProductList.getLayout = (page) => <AdminPanelLayout>{page}</AdminPanelLayout>;
 
 export default ProductList;
